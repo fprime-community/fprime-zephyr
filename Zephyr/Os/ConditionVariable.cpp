d4c7342bbec6ff80a2e373c3021696d216ba39b7
vibai <vivian@aspadeco.com>
2025-05-20 16:54:47 -0700

    Add Os ConditionVariable module for MutexSupport

---
+++ Zephyr/Os/ConditionVariable.cpp
@@ -0,0 +1,38 @@
+#include "Zephyr/Os/ConditionVariable.hpp"
+#include "Fw/Types/Assert.hpp"
+#include "Fw/Logger/Logger.hpp"
+#include "Zephyr/Os/Mutex.hpp"
+
+//https://docs.zephyrproject.org/apidoc/latest/group__condvar__apis.html
+
+namespace Os {
+namespace Zephyr {
+namespace Mutex {
+
+ZephyrConditionVariable::ZephyrConditionVariable() {
+    int status = k_condvar_init(this->m_handle.m_condition);
+    FW_ASSERT(status == 0, static_cast<FwAssertArgType>(status));  // If this fails, something horrible happened.
+}
+ZephyrConditionVariable::~ZephyrConditionVariable() {
+}
+
+ZephyrConditionVariable::Status ZephyrConditionVariable::pend(Os::Mutex& mutex) {
+    ZephyrMutexHandle* mutex_handle = reinterpret_cast<ZephyrMutexHandle*>(mutex.getHandle());
+    int status = k_condvar_wait(this->m_handle.m_condition, mutex_handle->m_mutex_descriptor, K_FOREVER);
+    FW_ASSERT(status == 0, static_cast<FwAssertArgType>(status));  // If this fails, something horrible happened.
+}
+void ZephyrConditionVariable::notify() {
+    FW_ASSERT(k_condvar_signal(this->m_handle.m_condition) == 0);
+}
+void ZephyrConditionVariable::notifyAll() {
+    int status = k_condvar_broadcast(this->m_handle.m_condition);
+    Fw::Logger::log("Number of %d threads have succesfully woken up\n", status);
+}
+
+ConditionVariableHandle* ZephyrConditionVariable::getHandle() {
+    return &m_handle;
+}
+
+}  // namespace Mutex
+}  // namespace Zephyr
+}  // namespace Os
--- Zephyr/Os/DefaultMutex.cpp
+++ Zephyr/Os/DefaultMutex.cpp
@@ -3,15 +3,15 @@
 // \brief sets default Os::Mutex Posix implementation via linker
 // ======================================================================
 #include "Os/Delegate.hpp"
-#include "Os/Posix/ConditionVariable.hpp"
-#include "Os/Posix/Mutex.hpp"
+#include "Zephyr/Os/ConditionVariable.hpp"
+#include "Zephyr/Os/Mutex.hpp"
 namespace Os {
 
 //! \brief get a delegate for MutexInterface that intercepts calls for Posix
 //! \param aligned_new_memory: aligned memory to fill
 //! \return: pointer to delegate
 MutexInterface* MutexInterface::getDelegate(MutexHandleStorage& aligned_new_memory) {
-    return Os::Delegate::makeDelegate<MutexInterface, Os::Posix::Mutex::PosixMutex>(aligned_new_memory);
+    return Os::Delegate::makeDelegate<MutexInterface, Os::Zephyr::Mutex::ZephyrMutex>(aligned_new_memory);
 }
 
 //! \brief get a delegate for MutexInterface that intercepts calls for Posix
@@ -19,7 +19,7 @@ MutexInterface* MutexInterface::getDelegate(MutexHandleStorage& aligned_new_memo
 //! \return: pointer to delegate
 ConditionVariableInterface* ConditionVariableInterface::getDelegate(
     ConditionVariableHandleStorage& aligned_new_memory) {
-    return Os::Delegate::makeDelegate<ConditionVariableInterface, Os::Posix::Mutex::PosixConditionVariable,
+    return Os::Delegate::makeDelegate<ConditionVariableInterface, Os::Zephyr::Mutex::ZephyrConditionVariable,
                                       ConditionVariableHandleStorage>(aligned_new_memory);
 }
 }  // namespace Os
